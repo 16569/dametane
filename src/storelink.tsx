38f8f900092df18fc5f5a0392f5820f07746a4e2
16569 <osamu_cyber@me.com>
2022-06-04 23:50:43 +0900

    AppStoreリンク

--- src/storelink.tsx
+++ src/storelink.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 export default function AppStore() {
-    let html: string = "<a href=\"https://apps.apple.com/jp/app/effortvaluetool/id1609571317?itsct=apps_box_badge&amp;itscg=30200\" style=\"display: inline-block; overflow: hidden; border-radius: 13px; width: 120px; height: 83px;\"><img src=\"https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1654300800&h=d379166e3261cffc9318e42725545164\" alt=\"Download on the App Store\" style=\"border-radius: 13px; width: 120px; height: 83px;\"></a>"
+    let html: string = "<a href=\"https://apps.apple.com/jp/app/effortvaluetool/id1609571317?itsct=apps_box_badge&amp;itscg=30200\" style=\"display: inline-block; overflow: hidden; border-radius: 13px; width: 120px; height: 60px;\"><img src=\"https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1654300800&h=d379166e3261cffc9318e42725545164\" alt=\"Download on the App Store\" style=\"border-radius: 13px; width: 120px; height: 60px;\"></a>"
     return (
         <React.Fragment>
          <div dangerouslySetInnerHTML={{__html: html}}></div>
